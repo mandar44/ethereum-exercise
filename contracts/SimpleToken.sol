34f3faa8230c35c8eb39297d293a13f13d3cf66c
Tony Onejohi <tonniewanjohi@gmail.com>
2019-01-25 17:59:53 +0300

    Completed exercice

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -4,48 +4,55 @@ pragma solidity >=0.4.21 < 0.6.0;
 contract SimpleToken {
 
     //create an array with all balances
-    mapping (address => uint) public balances;
-    address public owner;
-    uint public initialSupply = 5400000;
+    mapping (address => int) public balances;
+    address payable public owner;
+    int public initialSupply = 5400000; // initialize supply to 5400000 units (tokens)
 
     // log the transfer coin from one address to another
-    event TransferToken(address indexed _from, address indexed _to, uint amount);
+    event TransferToken(address indexed _from, address indexed _to, int amount);
 
     constructor () public payable {
-        //require(msg.value == 350 ether, "350 (5.4 million tokens) ether initial funding required");
         /* set the owner to the creator of this contract */
-        balances[tx.origin] = initialSupply;
+        balances[msg.sender] = initialSupply;
     }
 
     // send coin from account to account and return amount of tokens transfered
-    function transferCoin(address reciever, uint amount) public returns (uint token) {
+    function transferCoin(address reciever, int amount) public returns (int token) {
         // If the amount is more than what user is trying to send, return sufficient = false
         // amount is in Eth, convert to tokens to sent to acc
-        uint tokens = convertToTokens(amount, 15400);
+        int tokens = convertToTokens(amount, 15400);
         bool valid = validateTransaction(tokens);
 
         if( valid == true ) {
-            balances[msg.sender] -= tokens;
-            balances[reciever] += tokens;
+            balances[msg.sender] -= int(tokens);
+            balances[reciever] += int(tokens);
             emit TransferToken(msg.sender, reciever, tokens);
-            return tokens;
+            return int(tokens);
+        } else {
+            // transaction invalid, return 0
+            return 0;
         }
     }
 
-    function validateTransaction (uint amount) public view returns (bool sufficient) {
+    function validateTransaction (int amount) public view returns (bool sufficient) {
         // check if sender has enough tokens to send to account
         if (balances[msg.sender] < amount) return false;
         else return true;
     }
 
-    function convertToTokens(uint amount, uint conversionRate) public pure returns (uint convertedVal) {
+    function convertToTokens(int amount, int conversionRate) public pure returns (int convertedVal) {
         // multiply by the conversion rate to get value of tokens equivalent to amount of ether sent
         return amount * conversionRate;
     }
 
     // show account balance
-    function getBalance(address addr) public view returns (uint256) {
-        return balances[addr];
+    function getBalance(address addr) public view returns (int) {
+        return int(balances[addr]);
+    }
+
+    // get contract description
+    function getContractDescription() public view returns (address) {
+        return address(msg.sender);
     }
 
 }
